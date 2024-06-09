9fcb531d55db14bc3a414c0efae93168fcf521ae
Valente Licona <isc.lcv@gmail.com>
2024-06-09 06:46:57 -0600

    [ADD] Add Astro Islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astro-blog-vlc.netlify.app/"
+  site: "https://astro-blog-vlc.netlify.app/",
+  integrations: [preact()]
 });
